a32922017600d9190ef05733547d186c578ccd1d
João Vitor Alvares Freitas <joao.freitas@onfly.com.br>
2024-12-03 17:55:52 -0300

    test: creates CustomerTest

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,7 +6,9 @@ use App\Models\Vehicle\Brand;
 use App\Models\Vehicle\Category;
 use App\Models\Vehicle\Vehicle;
 use App\Models\Vehicle\VehicleModel;
+use Database\Factories\CompanyFactory;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -18,6 +20,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Company extends Model
 {
+    /** @use HasFactory<CompanyFactory> */
+    use HasFactory;
+
     protected $table = 'companies';
 
     protected $fillable = [
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Database\Factories\CustomerFactory;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
@@ -12,16 +15,19 @@ use Illuminate\Support\Str;
  * @property int $user_id
  * @property int $company_id
  * @property string $marital_status
- * @property string $birthday
+ * @property Carbon $birthday
  * @property string $gender
  * @property string $profession
  * @property string $cnh_number
  * @property string $cnh_security_code
  * @property string $cnh_category
- * @property string $cnh_expiration_date
+ * @property Carbon $cnh_expiration_date
  */
 class Customer extends Model
 {
+    /** @use HasFactory<CustomerFactory> */
+    use HasFactory;
+
     protected $table = 'customers';
 
     protected $fillable = [
@@ -63,4 +69,9 @@ class Customer extends Model
             set: fn($value) => Str::upper($value),
         );
     }
+
+    protected $casts = [
+        'birthday' => 'date',
+        'cnh_expiration_date' => 'date',
+    ];
 }
